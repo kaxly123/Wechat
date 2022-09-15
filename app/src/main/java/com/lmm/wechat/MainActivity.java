39cfef4a1ff7b5c51c9665bccf578d1eef1f6eee
kaxly <2265724054@qq.com>
2022-09-15 19:52:56 +0800

    安卓实现的简单微信界面

--- app/src/main/java/com/lmm/wechat/MainActivity.java
+++ app/src/main/java/com/lmm/wechat/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.me);
+        System.out.println("hello");
     }
 }
